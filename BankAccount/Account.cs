8e8dfa4d20a29f307186ddaf4616366115aff271
Joe Santoiemmo <JSanto333@gmail.com>
2017-07-16 22:38:52 -0400

    Working on Bank Account Project

--- BankAccount/Account.cs
+++ BankAccount/Account.cs
@@ -8,6 +8,55 @@ namespace BankAccount
 {
     class Account
     {
+        //one private instance of the variable decimal to represent the account balance
+        private decimal _balance;
+        
+        //constructor that receives an initial balance and uses it to initialize the instance variable public property
+        public Account(decimal balance)
+        {
+            Balance = balance;
+        }
 
+        //The property should validate the initial balance
+        public decimal Balance
+        {
+            get { return _balance; }
+            set
+            {
+                if (value >= 0)
+                    _balance = value;
+                else
+                    throw new Exception("Balance cannot be negative");
+            }
+        }
+
+        //Method Credit should add an amount to the current balance
+        public virtual void Credit(decimal amount)
+        {
+            if (amount > 0)
+                Balance += amount;
+            else
+                throw new Exception("Credited amount must be greater than zero");
+        }
+
+        //Method Debit should withdraw money from the Account and ensure that the debit amount does not exceed the
+        //Account's balance. If it does, the balance should be left unchanged, and the method should display
+        //the message "Debit amount exceeded account balance."
+        public virtual bool Debit(decimal amount)
+        {
+            bool OK = true;
+            if (Balance - amount >= 0)
+            {
+                Balance -= amount;
+                OK = true;
+            }
+            else
+            {
+                Console.Write("Debit amount exceeded account balance.");
+                OK = false;
+            }
+
+            return OK;
+        }
     }
 }
---
+++ BankAccount/CheckingAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    //CheckingAccount should inherit from base class Account and include a decimal instance
+    //variable that represents the fee charged per transaction.
+    class CheckingAccount : Account
+    {
+        private decimal _fee;
+
+        //constructor should receive the initial balance, as well as a parameter indicating a fee amount.
+        public CheckingAccount(decimal balance, decimal fee) : base(balance)
+        {
+
+        }
+
+        public decimal Fee
+        {
+            get { return _fee; }
+            set
+            {
+
+            }
+        }
+    }
+}
